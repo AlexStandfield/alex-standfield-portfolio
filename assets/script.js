914c870cd3fb75847e7f277023780f4320cc17b4
Alex Standfield <AlexStandfield@gmail.com>
2022-05-05 10:51:11 -0600

    added Drunk Trivia Project

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,13 @@ showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("project-images");
+  let slides = document.getElementsByClassName("oc-project-images");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -25,4 +23,31 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+// Drunk Trivia Slideshow
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlide2s(slideIndex2 = n);
+}
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("dt-project-images");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
 }
